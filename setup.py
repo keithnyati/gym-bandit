448b0f2ca69fc3a64acf2aad032181d758167a49
beaver <keithtnyati@gmail.com>
2019-04-06 17:55:11 +0200

    Update version 'setup.py'

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_bandit',
-      version='0.0.2',
+      version='0.0.3',
       install_requires=['gym', 'numpy'] # And any other dependencies required
 )
