789b3be5dde689f30644ed0a75cb73b6e6219266
beaver <keithtnyati@gmail.com>
2019-04-06 17:53:56 +0200

    Modify greedy & gradient module init

--- gym_bandit/envs/__init__.py
+++ gym_bandit/envs/__init__.py
@@ -1,3 +1,3 @@
 # Keep an eye on this may be deprecated in future
-from gym_bandit.envs.bandit_env import GreedyBanditEnv
-from gym_bandit.envs.bandit_env import GradientBanditEnv
+from gym_bandit.envs.greedy import GreedyBanditEnv
+from gym_bandit.envs.gradient import GradientBanditEnv
